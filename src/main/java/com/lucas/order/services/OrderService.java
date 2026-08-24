285027fbebad7d914f6d6dd43ff025993114b60a
Lucas Lima <lucas.luk.lima@gmail.com>
2026-08-23 22:17:14 -0300

    feat: implementar cálculos de frete e desconto

---
+++ src/main/java/com/lucas/order/services/OrderService.java
@@ -0,0 +1,18 @@
+package com.lucas.order.services;
+
+import com.lucas.order.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total (Order order) {
+        return order.getBasic()
+                * (1 - order.getDiscount() / 100)
+                + shippingService.shipment(order);
+    }
+}
