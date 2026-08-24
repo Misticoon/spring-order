b9ff76b7507481259e504c903a67e081b67b58d9
Lucas Lima <lucas.luk.lima@gmail.com>
2026-08-23 22:57:59 -0300

    feat: implement order calculation

--- src/main/java/com/lucas/order/OrderApplication.java
+++ src/main/java/com/lucas/order/OrderApplication.java
@@ -1,11 +1,20 @@
 package com.lucas.order;
 
+import com.lucas.order.entities.Order;
+import com.lucas.order.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class OrderApplication implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
@@ -13,6 +22,17 @@ public class OrderApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		Integer code = sc.nextInt();
+		Double basic = sc.nextDouble();
+		Double discount = sc.nextDouble();
+
+		Order order = new Order(code, basic, discount);
+		System.out.printf("%nPedido código %d%n", order.getCode());
+		System.out.printf("Valor Total: R$ %.2f%n", orderService.total(order));
+
+		sc.close();
 	}
 }
